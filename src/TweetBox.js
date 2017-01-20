149bf6a5fe1e038f89d0fd06e7231078e7817590
Ryan Lau <rylau@thoughtworks.com>
2017-01-20 15:44:24 -0600

    Use React component style

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,58 +1,140 @@
+/*
+  In this module we have converted the TweetBox Component to an ES6 class that
+  extends from React.Component so that it can inherit some methods that provide
+  a small, strict API for managing state changes within React apps.
 
-const postTweet = function(tweet) {
-  // 5. The value of the textarea is logged to the console.
+  This implementation is superior to the previous version of TweetBox, which used
+  `ref`. That version is more similar to an older style of JavaScript that relies
+  on DOM elements for storing state. This style gives us more control and a better
+  separation of state and UI.
+
+  The implementation below is unfinished. There are a few TODOs below that you must
+  finish in order to complete this module.
+
+  At any time, you can use the npm scripts we wrote in the last module to re-compile
+  the app to check your syntax, and then refresh the browser to see changes.
+
+  You may prefer to make a copy of this file and then remove all the comments from
+  one of the copies to unclutter it a bit.
+*/
+
+
+const postTweet = (tweet) => {
   console.log(tweet);
 };
 
 /**
-  Implementation of a React "Uncontrolled Component." See below for more info.
+  All React components should begin as pure functional components like the previous
+  version of TweetBox. Only when a component needs to modify its own state should
+  it be converted to an ES6 class extending from React.Component.
 
-  The numbered steps outline the flow of the textarea's text value.
+  Notice that even though the TweetBox contains the `placeholder` prop, it is never
+  mutated from within the component. It should remain a prop and should NOT be added
+  to the mutable state.
+
+  You should ALWAYS prefer props over mutable state. Most of your app should be
+  controlled via props passed into components.
+
+  - API for React Components
+    https://facebook.github.io/react/docs/react-component.html
+
+  - Best practices for using form fields in React
+    http://goshakkk.name/controlled-vs-uncontrolled-inputs-react/
+    https://facebook.github.io/react/docs/forms.html#controlled-components
 */
-const TweetBox = (props) => {
-  const placeholder = props.placeholder;
+class TweetBox extends React.Component {
+  constructor(props) {
+    super(props);
 
-  // 1. Declare a constiable that will eventually be assigned the <textarea> DOM element
-  var tweetInput;
+    /*
+      `this.state` is the the private, mutable state of this component.
 
-  const handleClick = () => {
-    // 4. Access the value of the textarea
-    postTweet(tweetInput.value);
-    tweetInput.value = "";
-  };
+      Here, the state of the component is represented by a single JavaScript object.
+      The value `tweet` key is the mutable value of the textarea, representing the
+      text of the tweet. Initialliy it is an empty string.
+    */
+    this.state = {
+      tweet: ""
+    };
 
-  return (
-    <div>
-      <textarea rows="5" cols="60" ref={(textareaElement) => {tweetInput = textareaElement}}/>
-      <input type="button" value="Tweet" onClick={handleClick} />
-    </div>
-  );
+    /*
+      This part is a small JavaScript oddity with out the `this` keyword is handled with
+      React and ES6 classes.
+
+      It is a bit confusing and I wouldn't worry too much about this part.
+
+      http://egorsmirnov.me/2015/08/16/react-and-es6-part3.html
+    */
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /**
+    Callback when the textarea's value is changed.
+  */
+  handleChange(event) {
+    /*
+      When invoked, `event.target.value` is the current value of the textarea. We
+      need to modify the component's state here, which will eventually lead to a
+      re-render and a UI update.
+
+      API docs and example: https://facebook.github.io/react/docs/react-component.html#setstate
+
+      TODO: Use `this.setState({tweet: <new value>});` to change the state of the component.
+    */
+    this.setState({tweet: event.target.value});
+  }
+
+  /**
+    Callback when the Tweet button is clicked.
+  */
+  handleSubmit() {
+    /*
+      When invoked, we need to post the tweet and reset the component's state back
+      to empty so the textarea field is cleared.
+
+      TODO: First, call the `postTweet` function, passing the current tweet state
+        as the argument. You are allowed to get the state value using `this.state.tweet`
+
+      TODO: Second, reset the tweet text back to empty string using `this.setState(<new state>)`
+    */
+    postTweet(this.state.tweet);
+    this.setState({tweet: ""});
+  }
+
+  /**
+    React.Component's render method. Returns a single React element.
+
+    https://facebook.github.io/react/docs/react-component.html#render
+  */
+  render() {
+    /*
+     We are now using ES6 Destructuring Assignment syntax to destructure the props
+     into individual variables.
+    */
+    const {placeholder} = this.props;
+
+    return (
+      <div>
+        {/* The textarea's value comes from the component's current state every time the component is rendered. */}
+        <textarea placeholder={placeholder} value={this.state.tweet} rows="5" cols="60" onChange={this.handleChange} />
+        <div>
+          {
+            /*
+              TODO: Render the character count (length) of the current tweet state here
+
+              In JSX, you can interpolate JavaScript by using curly braces.
+              For example:
+              const hello = (name) => {
+                  return <p>Hello {name}!</p>;
+              };
+            */
+
+          }
+          <p>{this.state.tweet.length}</p>
+          <input type="button" value="Tweet" onClick={this.handleSubmit} />
+        </div>
+      </div>
+    );
+  }
 };
-
-/*
-  React supports a special attribute that you can attach to any component. The "ref"
-  attribute takes a callback function, and the callback will be executed immediately after
-  the component is mounted or unmounted.
-
-  From the callback, we can get ahold of the actual DOM element being created and
-  get/modify its attributes, such as value.
-
-  This is necessary because "tweetText" is a React element, NOT a <textarea> input field.
-
-  React refers to this pattern as an Uncontrolled Component, meaning that React does
-  not control the state of the component.
-
-  Using ref is totally acceptable for something small like this, but the advice is
-  not to overuse it because it tightly couples you to the DOM. We will see another
-  way of doing this later.
-
-  https://facebook.github.io/react/docs/uncontrolled-components.html
-  https://facebook.github.io/react/docs/refs-and-the-dom.html
-*/
-
-/*
-<div>
-  <textarea rows=5 cols=60 />
-  <input type="button" value="Tweet"/>w
-</div>
-*/
